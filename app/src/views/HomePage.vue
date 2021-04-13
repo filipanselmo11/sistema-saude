2e4cd06cd0c24bf3de6fb1b57bce8fce69844d38
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-04-13 19:26:34 -0400

    adicionando o botão na tela de HomePage

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="container d-flex justify-content-around mt-5" style="width: 18rem;">
-    <div class="card-body d-flex justify-content-center">
-      <h5 class="card-title">Sistema De Gerenciamento de Exames Médicos Baseado em Blockchain</h5>
+  <div class="container d-flex justify-content-around mt-5">
+    <div class="card shadow-lg mr-5 mt-5" style="width: 18rem">
+      <div class="card-body d-flex justify-content-center">
+        <h5 class="card-title">SGEMBB</h5>
+      </div>
       <a href="https://dapp-uea-v0-0-1-filipanselmo11.vercel.app/" class="btn btn-primary">Acessar</a>
     </div>
   </div>
