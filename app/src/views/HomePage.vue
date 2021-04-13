523431ad6f2c880b7cb4772cd1064e59aa30d9f1
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-04-13 19:05:25 -0400

    Subindo a aplicação

---
+++ app/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container d-flex justify-content-around mt-5" style="width: 18rem;">
+    <div class="card-body d-flex justify-content-center">
+      <h5 class="card-title">Sistema De Gerenciamento de Exames Médicos Baseado em Blockchain</h5>
+      <a href="https://dapp-uea-v0-0-1-filipanselmo11.vercel.app/" class="btn btn-primary">Acessar</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
